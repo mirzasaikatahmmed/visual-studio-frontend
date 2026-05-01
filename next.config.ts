9009ca2fa1af80c72b89b5ac910a62f081d83970
mirzasaikatahmmed <mirzasaikatahmmed@gmail.com>
2026-05-02 05:32:31 +0600

    ✨ feat: Implement Google Reviews for testimonials

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "http", hostname: "localhost", port: "3001" },
-      { protocol: "https", hostname: "images.pixieset.com" },
-      { protocol: "https", hostname: "i.pravatar.cc" },
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "logo.clearbit.com" },
-      { protocol: "https", hostname: "visualstudioslens.saikat.com.bd" },
+      { protocol: "http", hostname: "**" },
+      { protocol: "https", hostname: "**" },
     ],
   },
 };
